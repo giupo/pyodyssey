798b10bb767ee1350cc1c6b6be0f62f70bb59eb2
Giuseppe Acito <giuseppe.acito@bancaditalia.it>
2013-11-22 10:22:36 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyOdyssey',
-    version='0.3.0',
+    version='0.3.1',
     author='Giuseppe Acito',
     author_email='giuseppe.acito@gmail.com',
     packages = find_packages(exclude=['test']),
